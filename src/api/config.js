c733d8423d2293a941a35d69fa22c964843099fe
samumirandam <samuelchampi@gmail.com>
2022-08-25 17:55:45 -0500

    fix: fixed readme production url and api url

--- src/api/config.js
+++ src/api/config.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://hn.algolia.com/api/v1";
+const API_URL = "https://hn.algolia.com/api/v1";
 
 const axiosInstance = axios.create({
   baseURL: API_URL,
